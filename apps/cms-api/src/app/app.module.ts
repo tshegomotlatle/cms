37a0fd1b2191416f5dc770449e94c8ca77a67596
tshegomotlatle <tshegomotlatle87@gmail.com>
2024-06-09 14:31:10 +0200

    🐛

--- apps/cms-api/src/app/app.module.ts
+++ apps/cms-api/src/app/app.module.ts
@@ -6,9 +6,14 @@ import { AuthenticationApiModule } from '@cms-authetication-api';
 import { CourtCasesApiModule } from '@cms-court-cases-api';
 import { DocumentsApiModule } from '@cms-documents-api';
 import { InvoicesApiModule } from '@cms-invoices-api';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthenticationApiModule, CourtCasesApiModule, DocumentsApiModule, InvoicesApiModule],
+  imports: [AuthenticationApiModule, CourtCasesApiModule, DocumentsApiModule, InvoicesApiModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'cms-api'),
+    exclude: ['/api/(.*)'],
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
