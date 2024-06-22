c6efd7611080758afffcc2bbe985b1c95aa6bbd6
tshegomotlatle <tshegomotlatle87@gmail.com>
2024-06-22 07:37:47 +0200

    🐛 Fix vitest errors

--- libs/cms-ui-library/src/lib/calendar/calendar-ui.spec.tsx
+++ libs/cms-ui-library/src/lib/calendar/calendar-ui.spec.tsx
@@ -2,9 +2,12 @@ import { render } from '@testing-library/react';
 
 import CalendarUi from './calendar-ui';
 import React from 'react';
+import axios from 'axios';
+import { vi } from 'vitest';
 
 describe('CalendarUi', () => {
   it('should render successfully', () => {
+    vi.spyOn(axios, 'post').mockResolvedValue([]);
     const { baseElement } = render(<CalendarUi />);
     expect(baseElement).toBeTruthy();
   });
