46e48fcfef7a186a4cb73972e1fd5cfea741514f
tshegomotlatle <tshegomotlatle87@gmail.com>
2024-06-22 07:33:50 +0200

    🐛

--- libs/cms-ui-library/src/lib/navigation/navigation.spec.tsx
+++ libs/cms-ui-library/src/lib/navigation/navigation.spec.tsx
@@ -1,9 +1,12 @@
 import { render } from '@testing-library/react';
 import Navigation from '../navigation/navigation';
 import React from 'react';
+import axios from 'axios';
+import { vi } from 'vitest';
 
 describe('Navigation', () => {
   it('should render successfully', () => {
+    vi.spyOn(axios, 'post').mockResolvedValue([]);
     const { baseElement } = render(<Navigation />);
     expect(baseElement).toBeTruthy();
   });
