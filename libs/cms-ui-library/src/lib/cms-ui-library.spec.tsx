7e86d81d77de00d1d28219c6a7292a6d12cbd00e
tshegomotlatle <tshegomotlatle87@gmail.com>
2024-06-22 07:34:30 +0200

    🐛

--- libs/cms-ui-library/src/lib/cms-ui-library.spec.tsx
+++ libs/cms-ui-library/src/lib/cms-ui-library.spec.tsx
@@ -2,9 +2,12 @@ import { render } from '@testing-library/react';
 
 import CmsUiLibrary from './cms-ui-library';
 import React from 'react';
+import axios from 'axios';
+import { vi } from 'vitest';
 
 describe('CmsUiLibrary', () => {
   it('should render successfully', () => {
+    vi.spyOn(axios, 'post').mockResolvedValue([]);
     const { baseElement } = render(<CmsUiLibrary />);
     expect(baseElement).toBeTruthy();
   });
