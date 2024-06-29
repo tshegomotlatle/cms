84f4a9ea2c257b619267cc442a0a479d98be75f4
tshegomotlatle <tshegomotlatle87@gmail.com>
2024-06-29 16:55:15 +0200

    ⚰️ remove common library

--- libs/cms-api-library/common/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/common.service';
-export * from './lib/common.module';
--- libs/cms-api-library/common/src/lib/common.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CommonService } from './common.service';
-
-@Module({
-  controllers: [],
-  providers: [CommonService],
-  exports: [CommonService],
-})
-export class CommonModule {}
