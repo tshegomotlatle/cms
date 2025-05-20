27ce45264136330a63bf4a9614d1c39713af5915
Tshegofatsho <tshegomotlatle87@gmail.com>
2025-05-20 22:42:19 +0200

    🐛 bring keyclock back to docker compose

--- libs/cms-api-library/authentication/authentication-api/src/lib/config/keycloak-config.service.ts
+++ libs/cms-api-library/authentication/authentication-api/src/lib/config/keycloak-config.service.ts
@@ -10,8 +10,8 @@ export class KeycloakConfigService implements KeycloakConnectOptionsFactory {
       authServerUrl: "http://localhost:8080", //your URL Keycloak server
       realm: "cms-realm", //realms that used for this app
       clientId: "cms-api", //client id for this app
-      secret: "Dn5cZaeRlVo3WewuEvF0LnQC3LjHHrEH", //secret for this app
-      realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoOsFsenf0jDXQmfB0+D5IiSvegEFq4LnDtGp2BbIm1dm2lxN6yOd+O3I8y88sgLfOYZZVqPPxXwg6riNWd2EA9hvOLxG8+D39Ks5345j7ErgET/2S4Kk8T+tiLYJh9Bo+vR8Z7zEBOFeg64bhkYX6NgSmxYpcO32FfFbqXgc7iRENl6exbzjn0+p8pqbwwmbS88146kKWIRimNNA2kRXp2cPBOMBaQn+Yezqax4LO0qzY7ArKQETmpyxFkQ96QA8ZOmEziIlmr1PTzSZJdGo0Sfue0j26zcX9eqbBOBxyTqGvD/Bwb/odm/1/hOjGfb9Kq2MlnoAWlRgA7GQVdG0zwIDAQAB",
+      secret: "Q3oyTVl2LOHBtQisbQFkVlJ0lU5rWhLW", //secret for this app
+      realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA21DiMR0ii3mRzv1HgJhV45OO6pnDXbSef9Spic1gQcDLkAMoE2wccEoY9LlNpR7Od+JBbgGqI6SVM8f5Y4iPrahvVFsC6/KnDyFIeEl/Z3+YAqEAlPSLRrNmzmOZY7qchqc2ZoyIwTNDGmWHGbnzMVYvlwxYgLr+ai7NjtGsODTpXMIFjNFmuTGTJlp5+TX6y6uPptP0x909VfPPeYI0nnIkAFjddJR9XtHL/Zw7nxMLFbOep7E/CySLlrJ8kAm3/t67ZsmQiDoxcVzGqAetH3R9/UrFY/cDxjhPtXkCGG6wVeCd3U46Rlp9MetdVA837rkvnlW/2qUGrb7uRlTBzQIDAQAB",
       logLevels: ['verbose'],
       
       // Configure token validation
